c554dde1960ca4df4a18e2b51f5a65bf40fd5a5c
Corentin Closs <me@corentincloss.fr>
2020-01-28 17:50:13 +0100

    Creating new subdomains from the API, return all known subdomains

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const bodyParser = require('body-parser');
 const lowdb = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 global.shortid = require('shortid');
+
+var multer = require('multer');
+var upload = multer();
 
 const adapter = new FileSync('db.json');
 global.db = lowdb(adapter);
@@ -40,11 +44,15 @@ const getDurationInMilliseconds = (start) => {
     return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
 };
 
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(upload.array());
+
 app.use((req, res, next) => {
     const start = process.hrtime();
     res.on('finish', () => {
         const durationInMilliseconds = getDurationInMilliseconds (start);
-        console.log(`[${req.method}] ${res.statusCode} ${req.originalUrl} ${durationInMilliseconds .toLocaleString()} ms`);
+        console.log(`${req.ip} -> [${req.method}] ${res.statusCode} ${req.originalUrl} ${durationInMilliseconds .toLocaleString()} ms`);
     });
     next();
 });
--- modules/subdomains.js
+++ modules/subdomains.js
@@ -1,10 +1,6 @@
 const subdomains = require('express').Router();
 
-subdomains.get('/', function (req, res) {
-    res.sendStatus(200);
-});
-
-subdomains.post('/refresh', function (req, res) {
+function refreshSubdomains(){
     let domains = db.get('domains').value();
     for (let i = 0; i < domains.length; i++) {
         cfInstance.get("https://api.cloudflare.com/client/v4/zones/"+domains[i]['domain_id']+"/dns_records").then(function (response) {
@@ -12,19 +8,63 @@ subdomains.post('/refresh', function (req, res) {
             for (let j = 0; j < response.length; j++) {
                 if (db.get('subdomains').find({subdomain : response[j]['name']}).value() === undefined && response[j]['type'] === "A" || response[j]['type'] === "CNAME"){
                     db.get('subdomains').push({
-                            id: shortid.generate(),
-                            subdomain: response[j]['name'],
-                            subdomain_id: response[j]['id'],
-                            parent_domain_id: domains[i]["id"],
-                            target: response[j]["content"],
-                            type: response[j]["type"],
-                            ignored: 0
-                        }).write();
+                        id: shortid.generate(),
+                        subdomain: response[j]['name'],
+                        subdomain_id: response[j]['id'],
+                        parent_domain_id: domains[i]["id"],
+                        target: response[j]["content"],
+                        type: response[j]["type"],
+                        ignored: 0
+                    }).write();
                 }
             }
         })
     }
+}
+
+subdomains.get('/', function (req, res) {
+    let domainsList = db.get('domains').value();
+    let jsonReply = {};
+    for (let i = 0; i < domainsList.length; i++) {
+        let domainId = domainsList[i]['id'];
+        jsonReply[domainsList[i]['domain']] = [];
+        let subdomains = db.get('subdomains').filter({parent_domain_id: domainId}).value();
+        for (let j = 0; j < subdomains.length; j++) {
+            jsonReply[domainsList[i]['domain']].push({id: subdomains[j]['id'], subdomain : subdomains[j]['subdomain'], target: subdomains[j]['target'], type: subdomains[j]['type']})
+        }
+    }
+    res.json(jsonReply);
+});
+
+subdomains.post('/refresh', function (req, res) {
+    refreshSubdomains();
     res.sendStatus(200)
+});
+
+subdomains.post('/new', function (req, res) {
+    let name = req.body.name, target = req.body.target, type = req.body.type;
+    if (name !== undefined || target !== undefined || type !== undefined){
+        if (db.get('subdomains').find({subdomain: name, }).value() === undefined){
+            let domainSplit = name.split("."), domainName = (domainSplit[domainSplit.length-2]+"."+domainSplit[(domainSplit.length-1)]);
+            if (db.get('domains').find({domain: domainName}).value() !== undefined){
+                let zoneId = db.get('domains').find({domain: domainName}).value()['domain_id'];
+                cfInstance.post("https://api.cloudflare.com/client/v4/zones/"+zoneId+"/dns_records", {
+                    type: type,
+                    name: name,
+                    content: target
+                }).then(function (response) {
+                    refreshSubdomains();
+                });
+                res.status(200).json({response: "Subdomain added"});
+            } else {
+                res.status(400).json({response: "Unknown domain name."});
+            }
+        } else {
+            res.status(400).json({response: "Subdomain already existing."});
+        }
+    } else {
+        res.status(400).json({response: "Missing one or more mandatory parameters."});
+    }
 });
 
 module.exports = subdomains;
